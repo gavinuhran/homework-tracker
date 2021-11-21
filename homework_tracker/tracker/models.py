1857dccd9f280963b36fea3105dea43f689f5e8a
Gavin Uhran <gavinu2001@gmail.com>
2021-11-21 15:46:34 -0500

    Created Task Model

---
+++ homework_tracker/tracker/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    STATUSES = (
+        ('N', 'Not Started'),
+        ('I', 'In Progress'),
+        ('C', 'Complete'),
+    )
+    title = models.CharField(max_length=200)
+    due_date = models.DateTimeField()
+    description = models.TextField()
+    status = models.CharField(max_length=1, choices=STATUSES)
+    total_time = models.IntegerField()
