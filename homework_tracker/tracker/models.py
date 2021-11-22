3f53467b1330d9c24ac19ff5dbb2ab84b894bd55
Gavin Uhran <gavinu2001@gmail.com>
2021-11-21 22:42:24 -0500

    Added create feature

--- homework_tracker/tracker/models.py
+++ homework_tracker/tracker/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import uuid
 # Create your models here.
 class Task(models.Model):
     STATUSES = (
@@ -7,9 +7,11 @@ class Task(models.Model):
         ('In Progress', 'In Progress'),
         ('Complete', 'Complete'),
     )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
     project = models.CharField(default="placeholder", max_length=100)
-    due_date = models.DateTimeField()
+    due_date = models.DateField()
     description = models.CharField(max_length=500)
     status = models.CharField(max_length=50, choices=STATUSES)
     total_time = models.IntegerField()
--- homework_tracker/tracker/views.py
+++ homework_tracker/tracker/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 from .models import Task
+import datetime
 
 # Create your views here.
 def index(request):
     tasks = Task.objects.all()
     return render(request, 'index.html', {'tasks': tasks})
+
+def create(request):
+    if request.method == 'POST':
+        task = Task()
+        task.title = request.POST.get('title')
+        task.project = request.POST.get('project')
+
+        # nums = [MM, DD, YYYY]
+        nums = [int(x) for x in request.POST.get('date').split('/')]
+        d = datetime.date(nums[2], nums[0], nums[1])
+        task.due_date = d
+
+        task.description = request.POST.get('description')
+        task.status = request.POST.get('status')
+        task.total_time = request.POST.get('total_time')
+        task.save()
+
+        tasks = sorted(Task.objects.all(), key=lambda x: x.due_date)
+        return render(request, 'index.html', {'tasks': tasks})
+    else:
+        return render(request, 'create.html', {})
